e4927d1fce374a77d5491628040b39b1d2f77805
jasnmar <jasnmar@gmail.com>
2024-12-02 15:07:03 -0500

    Day 2 complete(ish)

--- index.js
+++ index.js
@@ -1,81 +1,17 @@
-/*
-Grandpa has a Christmas wish list to keep track of all the gifts he wants to ask for. But there’s a problem: if he forgets he’s already added something, the list gets clogged up with duplicates. This happened last year, and he ended up with 8 talking picture frames on Christmas Day!
+const calendarContainer = document.getElementById('calendar');
 
-Your task is to complete the `checkDuplicate()` function 👇 to ensure no duplicates are added to the list. But here’s the tricky part: Grandpa sometimes hits the spacebar more than once, making it harder to spot duplicates.
-
-For example, only one of these entries should be added to the list — the others should be flagged as duplicates:
-
-- "talking picture frames"
-- "talking  picture frames"
-- "talking picture    frames"
-- " talking picture frames "
-
-**Your tasks:**
-1. Ensure no duplicates can be added to the list.
-2. Account for extra spaces at the beginning/end and between words.
- 
-**Stretch Goals:**
-1. Case Sensitivity: Handle cases where capitalization differs. For example:
-   - `"Cat Hammock"` should be flagged as a duplicate of `"cat hammock"`.
-   - Preserve Grandpa’s original capitalization (e.g., if `"Cat Hammock"` is added first, that should be added to the list). Do not simply convert all entries to lower case - Grandpa might well want to capitalize some words. 
-
-2. Additional Features: Add functionality to delete or edit items on the list.
-*/
-
-// Get references to DOM elements
-const itemInput = document.getElementById('item-input')
-const addItemButton = document.getElementById('add-item-button')
-const shoppingList = document.getElementById('shopping-list')
-const listArr = []
-
-// Function to check item is not duplicate
-function checkDuplicate() {
-    
-    const existingList = listArr.map((item) => {
-        return simplifyListItem(item)
-    })
-    
-    const itemText = itemInput.value
-    const simplifiedItem = simplifyListItem(itemText)
-    let matches = false
-    existingList.forEach((item) => {
-        if(JSON.stringify(item)===JSON.stringify(simplifiedItem)) {
-            matches = true
-        }
-    })
-    if(!matches) {
-        listArr.push(itemText)
-    }
-    renderList()
+for (let i = 1; i <= 24; i++) {
+  let box = document.createElement('li');
+  box.classList.add('calendar-box');
+  let number = document.createElement('p');
+  number.classList.add('date')
+  number.innerHTML = i;
+  const icon = document.createElement('i');
+  icon.classList.add('fas', 'fa-gift');
+  let description = document.createElement('p');
+  description.innerHTML = "Open me!";
+  box.appendChild(number);
+  box.appendChild(icon);
+  box.appendChild(description);
+  calendarContainer.appendChild(box);
 }
-
-function simplifyListItem(item) {
-    console.log(item)
-    const individualWords = item.split(' ')
-    const fixedWords = individualWords.filter((word) => word != "")
-    return fixedWords.map((word) => {
-      return word.toLowerCase()
-    })
-}
-
-
-// Function to add an item to the shopping list
-function renderList() {
-    shoppingList.innerHTML = ''
-    listArr.forEach((gift) => {
-        const listItem = document.createElement('li')
-        listItem.textContent = gift
-        shoppingList.appendChild(listItem)
-    })
-    itemInput.value = ''; // Clear the input field
-}
-
-// Add event listener to button
-addItemButton.addEventListener('click', checkDuplicate)
-
-// Allow adding items by pressing Enter key
-itemInput.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        checkDuplicate()
-    }
-})
