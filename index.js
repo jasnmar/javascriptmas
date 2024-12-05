ddf769b6d92e8dddb30b7f9819b5cfdd051cb8d1
jasnmar <jasnmar@gmail.com>
2024-12-05 11:42:42 -0500

    Working Version

--- index.js
+++ index.js
@@ -1,81 +1,126 @@
 /*
-Grandpa has a Christmas wish list to keep track of all the gifts he wants to ask for. But there’s a problem: if he forgets he’s already added something, the list gets clogged up with duplicates. This happened last year, and he ended up with 8 talking picture frames on Christmas Day!
+You are going to build an app that challenges players to identify a Christmas Movie from some emoji 🍿 🎅 🎬. The players will have 3 guesses per movie.
 
-Your task is to complete the `checkDuplicate()` function 👇 to ensure no duplicates are added to the list. But here’s the tricky part: Grandpa sometimes hits the spacebar more than once, making it harder to spot duplicates.
+For example, the emoji 🌇 💣 👮 ✈️ ️🔫  represent the film “Die Hard”, which everyone knows is the best Christmas movie of all time.
 
-For example, only one of these entries should be added to the list — the others should be flagged as duplicates:
+In data.js you have an array of Christmas movies with emoji and text for aria labels.
 
-- "talking picture frames"
-- "talking  picture frames"
-- "talking picture    frames"
-- " talking picture frames "
+Your task is to build an app that meets these criteria:
 
-**Your tasks:**
-1. Ensure no duplicates can be added to the list.
-2. Account for extra spaces at the beginning/end and between words.
- 
-**Stretch Goals:**
-1. Case Sensitivity: Handle cases where capitalization differs. For example:
-   - `"Cat Hammock"` should be flagged as a duplicate of `"cat hammock"`.
-   - Preserve Grandpa’s original capitalization (e.g., if `"Cat Hammock"` is added first, that should be added to the list). Do not simply convert all entries to lower case - Grandpa might well want to capitalize some words. 
+- The app should present the player with a set of emoji selected at random from the array in data.js. 
 
-2. Additional Features: Add functionality to delete or edit items on the list.
+- The player will input their guess.
+
+- If the player guesses correctly, the app should display a message saying "Correct!". Then, after a pause of 3 seconds, it should randomly select the next set of emoji clues and display them to the player.
+
+- If the player’s guess is incorrect, the app should display a message saying “Incorrect! You have 2 more guesses remaining.”
+
+- If the player fails to guess correctly on the next two attempts, the app should display a message saying, `The film was <Film Name Here>!`. After a pause of 3 seconds, it should randomly select a new set of emoji clues and display them to the player.
+
+- When all films in the array have been used, the player should see a message saying "That's all folks!".
+
+- Each film should only be used once. There should be no repetition. 
+
+
+Stretch Goals
+
+- Use AI to decide if an answer is correct or incorrect. For example if the correct answer is "The Polar Express" but the player inputs "Polar Express" a straight comparison of the two strings will find that the player's answer was incorrect. AI could assess if there is sufficient similarity between the strings to judge it as correct. 
+
+- Improve the UX by disabling the form/button when the game is over and during the pause between questions.
 */
 
-// Get references to DOM elements
-const itemInput = document.getElementById('item-input')
-const addItemButton = document.getElementById('add-item-button')
-const shoppingList = document.getElementById('shopping-list')
-const listArr = []
+import { films } from '/data.js'
 
-// Function to check item is not duplicate
-function checkDuplicate() {
-    
-    const existingList = listArr.map((item) => {
-        return simplifyListItem(item)
-    })
-    
-    const itemText = itemInput.value
-    const simplifiedItem = simplifyListItem(itemText)
-    let matches = false
-    existingList.forEach((item) => {
-        if(JSON.stringify(item)===JSON.stringify(simplifiedItem)) {
-            matches = true
-        }
-    })
-    if(!matches) {
-        listArr.push(itemText)
-    }
-    renderList()
+// Some useful elements
+const guessInput = document.getElementById('guess-input')
+const messageContainer = document.getElementsByClassName('message-container')[0]
+const emojiCluesContainer = document.getElementsByClassName('emoji-clues-container')[0]
+const guess = document.getElementsByName('guess-input')
+
+// State
+let cQuestionIndex = -1
+let cQuestionData = {}
+const qArray = []
+let guesses = 3
+const qTimeout = 1000
+
+guessInput.addEventListener('submit', (e) => checkAnswer(e))
+
+function checkAnswer(e) {
+  e.preventDefault()
+  console.log('guess: ', guess[0].value)
+  console.log('cQuestionData.title: ', cQuestionData.title)
+  const answer = guess[0].value
+  if(answer == cQuestionData.title) {
+    rightAnswer()
+  } else {
+    wrongAnswer()
+  }
 }
 
-function simplifyListItem(item) {
-    console.log(item)
-    const individualWords = item.split(' ')
-    const fixedWords = individualWords.filter((word) => word != "")
-    return fixedWords.map((word) => {
-      return word.toLowerCase()
-    })
+function rightAnswer() {
+  if(cQuestionIndex < films.length - 1) {
+    messageContainer.textContent = "Correct!"
+    setTimeout(() => {
+      getNextQuestion()
+    }, qTimeout);
+  } else {
+    messageContainer.textContent = "That's all folks!"
+  }
 }
 
-
-// Function to add an item to the shopping list
-function renderList() {
-    shoppingList.innerHTML = ''
-    listArr.forEach((gift) => {
-        const listItem = document.createElement('li')
-        listItem.textContent = gift
-        shoppingList.appendChild(listItem)
-    })
-    itemInput.value = ''; // Clear the input field
+function getNextQuestion() {
+  guesses = 3
+  cQuestionIndex++
+  cQuestionData = films[qArray[cQuestionIndex]]
+  createQuestion()
 }
 
-// Add event listener to button
-addItemButton.addEventListener('click', checkDuplicate)
+function createQuestion() {
+  const emojiList = cQuestionData.emoji.join(' ')
+  emojiCluesContainer.textContent = cQuestionData.emoji.join(' ')
+  let preText = 'Incorrect! '
+  if(guesses===3) {
+    preText = ''
+  }
+  messageContainer.textContent = `${preText}You have ${guesses} more guesses remaining.`
+}
 
-// Allow adding items by pressing Enter key
-itemInput.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        checkDuplicate()
-    }
-})
+function wrongAnswer() {
+  guesses--
+    if(guesses<1) {
+      messageContainer.textContent = `The film was ${cQuestionData.title}!`
+      if(cQuestionIndex < films.length - 1) {
+        setTimeout(() => {
+          getNextQuestion()
+        }, qTimeout)
+      } else {
+        messageContainer.textContent = "That's all folks!"
+      }
+  } else {
+    messageContainer.textContent = `Incorrect! You have ${guesses} more guesses remaining.`
+  }
+}
+
+questionList()
+getNextQuestion()
+
+//Setup Functions
+function questionList() {
+  console.log('films: ', films)
+  //create an array with all of the films by index
+  for(let i = 0; i < films.length; i++) {
+    qArray.push(i) 
+  }
+  shuffleArray(qArray)
+}
+
+function shuffleArray(array) {
+  for (let i = array.length -1 ; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i+1))
+    const tmp = array[i]
+    array[i] = array[j]
+    array[j] = tmp
+  }
+}
+
