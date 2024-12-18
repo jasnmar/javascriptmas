61dccb0abc4a1c02e0b34ca9b44e2811b6e576e4
jasnmar <jasnmar@gmail.com>
2024-12-18 10:57:47 -0500

    Completed

---
+++ index.js
@@ -0,0 +1,36 @@
+import shoppingCartData from "./data.js";
+
+/*
+😱 Christmas can get expensive! 
+
+You've been on a shopping trip and spent too much money. 
+But how much of that can you blame on Christmas?
+
+**Task**
+
+- Calculate and return the total cost of only the gifts in the shopping cart.
+- Each gift has the isGift boolean set to true.
+- The total cost of gifts should be given to two decimal places.
+
+Expected output: 559.93  
+
+**Stretch Goal**
+
+- Use the reduce() method to complete this challenge.
+*/
+
+function calculateCost(arr){
+    // Your code here!
+    const sum = arr.reduce((acc, cur) => {
+      if(cur.isGift) {
+        acc += cur.price
+      } else {
+        acc + 0
+      }
+      return acc
+    },0)
+    return sum.toFixed(2)
+}
+
+console.log(calculateCost(shoppingCartData)) //559.93 
+
